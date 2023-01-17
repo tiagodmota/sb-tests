98c9e038289eaac19a9da77f1c95aa5a072a6791
TiagoDM <tiago.motasaj@gmail.com>
2023-01-16 21:02:51 -0300

    first commit

---
+++ src/main/java/com/example/sbtests/SbTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbtests/SbTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
